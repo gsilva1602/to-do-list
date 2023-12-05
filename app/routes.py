a4ac696fdf0f140daf38b6754bdfda4cb5f30a13
Kevin Augusto Kadooca Bastos <Kevin@selettra.local>
2023-12-05 09:41:12 -0300

    first update

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template, request, redirect, url_for
+from app import app
+
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form['task']
+    tasks.append(task)
+    return redirect(url_for('index'))
+
+@app.route('/remove/<int:index>')
+def remove_task(index):
+    try:
+        task = tasks.pop(index)
+    except IndexError:
+        pass
+    return redirect(url_for('index'))
